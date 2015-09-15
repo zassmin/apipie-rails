4c4a53360415c51939797f969460b9c6163078cb
zassmin <zassmin@lumoslabs.com>
2015-09-15 11:29:44 -0700

    Initial pass at including an api console. Summary of changes: * use existing @doc data to generate templates for an api console form * use ajax to send form data to app's routes * use js to add response inline

--- app/public/apipie/javascripts/apipie.js
+++ app/public/apipie/javascripts/apipie.js
@@ -3,4 +3,53 @@ $(document).ready(function() {
     $('pre.ruby').addClass('prettyprint lang-rb');
     prettyPrint();
   }
+
+  $('form#api-params').on('submit', function(event) {
+    event.preventDefault();
+    var $form = $(this);
+    var actionTemplateUrl = $form.data('action-template');
+    var $urlParams = $('input[name^="url_params"]');
+
+    $urlParams.each(function(index, urlParam) {
+      var urlParamName = /url_params\[(.*)\]/.exec($(urlParam).attr('name'))[1];
+      actionTemplateUrl = actionTemplateUrl.replace(':' + urlParamName, $(urlParam).attr('value'));
+    });
+
+    $.ajax({
+      url: actionTemplateUrl,
+      type: $form.attr('method'),
+      data: $form.find('input').not('input[name^="url_params["]').serialize(),
+      dataType: "json",
+      success: function(data) {
+        $('#data-stuff code').html(JSON.stringify(data));
+        $('pre.ruby').addClass('prettyprint lang-rb');
+        prettyPrint();
+      },
+      error: function(data) {
+        $('#data-stuff code').html(JSON.stringify(data));
+        $('pre.ruby').addClass('prettyprint lang-rb');
+        prettyPrint();
+      }
+    });
+  });
+
+  $('#api-link').on('click', function(event) {
+    event.preventDefault();
+    var $form = $(this);
+
+    $.ajax({
+      url: $form.attr('href'),
+      type: $form.attr('data-method'),
+      success: function(data) {
+        $('#data-stuff code').html(JSON.stringify(data));
+        $('pre.ruby').addClass('prettyprint lang-rb');
+        prettyPrint();
+      },
+      error: function(data) {
+        $('#data-stuff code').html(JSON.stringify(data));
+        $('pre.ruby').addClass('prettyprint lang-rb');
+        prettyPrint();
+      }
+    });
+  })
 });
